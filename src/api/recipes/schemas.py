d0e75956054f68b704296d1d763adcdb25b7a4a4
G.Perdikaris <perdgeorge@gmail.com>
2025-11-26 16:50:11 +0200

    Fixing update schemas in order to not get nullable fields

--- src/api/recipes/schemas.py
+++ src/api/recipes/schemas.py
@@ -46,13 +46,11 @@ class GetRecipeSchema(RecipeBaseSchema):
 
 
 class UpdateRecipeSchema(BaseSchema):
-    name: str | None = Field(examples=["Tzatziki"], default=None)
-    cooking_time: int | None = Field(examples=[30], ge=1, default=None)
-    difficulty_level: DifficultyLevel | None = Field(
-        examples=["EASY", "MEDIUM", "HARD"], default=None
-    )
-    portions: int | None = Field(examples=[4], ge=1, default=None)
-    instructions: str | None = Field(examples=["Mix all ingredients."], default=None)
+    name: str = Field(..., examples=["Tzatziki"])
+    cooking_time: int = Field(..., examples=[30], ge=1)
+    difficulty_level: DifficultyLevel = Field(..., examples=["EASY", "MEDIUM", "HARD"])
+    portions: int = Field(..., examples=[4], ge=1)
+    instructions: str = Field(..., examples=["Mix all ingredients."])
     ingredients: list[RecipeIngredientPayload] = Field(
         default_factory=list, examples=[[{"ingredient_id": 1, "quantity": "100 grams"}]]
     )
