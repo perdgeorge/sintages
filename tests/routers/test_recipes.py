cffc77ae727ef6f3b7aadd0c562d142f9fc9968a
G.Perdikaris <perdgeorge@gmail.com>
2025-11-28 19:27:41 +0200

    Update for delete recipe and update recipe test

--- tests/routers/test_recipes.py
+++ tests/routers/test_recipes.py
@@ -1,16 +1,21 @@
 import pytest
 from fastapi.testclient import TestClient
 from src.db.models.recipes import Recipe
+from src.db.models.users import User
 from tests.factories import make_recipe_payload
 
 
 @pytest.mark.anyio
-def test_create_recipe(client: TestClient, user, ingredient_factory):
+def test_create_recipe(
+    client: TestClient, user: User, ingredient_factory, auth_headers: dict
+):
     ingredients = [ingredient_factory(), ingredient_factory()]
     ingredient_ids = [ingredient.id for ingredient in ingredients]
     payload = make_recipe_payload(user_id=user.id, ingredient_ids=ingredient_ids)
 
-    resp = client.post("/recipes", json=payload.model_dump(mode="json"))
+    resp = client.post(
+        "/recipes", json=payload.model_dump(mode="json"), headers=auth_headers
+    )
     assert resp.status_code == 201
     data = resp.json()
     assert data["name"] == payload.name.capitalize()
@@ -61,8 +66,8 @@ def test_get_user_recipes(client: TestClient, recipe_factory, user_factory):
 
 
 @pytest.mark.anyio
-def test_delete_recipe(client: TestClient, recipe: Recipe):
-    resp = client.delete(f"/recipes/{recipe.id}")
+def test_delete_recipe(client: TestClient, recipe: Recipe, auth_headers: dict):
+    resp = client.delete(f"/recipes/{recipe.id}", headers=auth_headers)
     assert resp.status_code == 200
     data = resp.json()
     assert data["id"] == recipe.id
@@ -71,3 +76,73 @@ def test_delete_recipe(client: TestClient, recipe: Recipe):
 
     follow_up = client.get(f"/recipes/{recipe.id}")
     assert follow_up.status_code == 404
+
+
+@pytest.mark.anyio
+def test_delete_recipe_forbidden_user(
+    client: TestClient, recipe: Recipe, other_auth_headers: dict
+):
+    resp = client.delete(f"/recipes/{recipe.id}", headers=other_auth_headers)
+    assert resp.status_code == 403
+
+    follow_up = client.get(f"/recipes/{recipe.id}")
+    assert follow_up.status_code == 200
+
+
+@pytest.mark.anyio
+def test_update_recipe(
+    client: TestClient,
+    recipe: Recipe,
+    user: User,
+    ingredient_factory,
+    auth_headers: dict,
+):
+    ingredients = [ingredient_factory(), ingredient_factory()]
+    ingredient_ids = [ingredient.id for ingredient in ingredients]
+    ingredient_names = [ingredient.name for ingredient in ingredients]
+    new_payload = make_recipe_payload(
+        user_id=user.id,
+        ingredient_ids=ingredient_ids,
+        ingredient_names=ingredient_names,
+    )
+    resp = client.put(
+        f"/recipes/{recipe.id}",
+        json=new_payload.model_dump(),
+        headers=auth_headers,
+    )
+    assert resp.status_code == 200
+    data = resp.json()
+    assert data["name"] == new_payload.name.capitalize()
+    assert data["cooking_time"] == new_payload.cooking_time
+    assert data["difficulty_level"] == new_payload.difficulty_level
+    assert data["portions"] == new_payload.portions
+    assert data["instructions"] == new_payload.instructions
+    expected_ingredients = [
+        {"ingredient_id": item.ingredient_id, "quantity": item.quantity}
+        for item in new_payload.ingredients
+    ]
+    assert data["ingredients"] == expected_ingredients
+
+
+@pytest.mark.anyio
+def test_update_recipe_forbidden_user(
+    client: TestClient,
+    recipe: Recipe,
+    user: User,
+    ingredient_factory,
+    other_auth_headers: dict,
+):
+    ingredients = [ingredient_factory(), ingredient_factory()]
+    ingredient_ids = [ingredient.id for ingredient in ingredients]
+    ingredient_names = [ingredient.name for ingredient in ingredients]
+    new_payload = make_recipe_payload(
+        user_id=user.id,
+        ingredient_ids=ingredient_ids,
+        ingredient_names=ingredient_names,
+    )
+    resp = client.put(
+        f"/recipes/{recipe.id}",
+        json=new_payload.model_dump(),
+        headers=other_auth_headers,
+    )
+    assert resp.status_code == 403
